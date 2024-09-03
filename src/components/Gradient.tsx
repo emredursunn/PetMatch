cb7bbde28500f8894603cc87944d5ed5ac58bc7e
emredursunn <emredursun908@gmail.com>
2024-09-03 23:28:28 +0300

    gradient background color for app

---
+++ src/components/Gradient.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { ReactNode } from "react";
+import { LinearGradient } from "expo-linear-gradient";
+
+type Props = {
+  children: ReactNode;
+};
+
+const Gradient = ({ children }: Props) => {
+  return (
+    <LinearGradient
+      style={{ flex: 1 }}
+      colors={["#CAD0FF", "#E3E3E3"]}
+      start={{ x: 0, y: 1 }}
+      end={{ x: 1, y: 1 }}
+    >
+      {children}
+    </LinearGradient>
+  );
+};
+
+export default Gradient;
+
+const styles = StyleSheet.create({});
+
+// colors={["#CAD0FF", "#E3E3E3"]}
