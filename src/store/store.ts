f90f2b83f8b2991560916f6184fa78c068c09225
emredursunn <emredursun908@gmail.com>
2024-09-05 19:57:27 +0300

    export appdispatch

--- src/store/store.ts
+++ src/store/store.ts
@@ -5,7 +5,10 @@ const store = configureStore({
   reducer: {
     auth: authReducer,
   },
-});
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
