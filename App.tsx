a680c91146e8b3457be76585e1174d1ec8d4c009
emredursunn <emredursun908@gmail.com>
2024-09-05 20:00:00 +0300

    firebase and redux initialized

--- App.tsx
+++ App.tsx
@@ -6,6 +6,8 @@ import Toast from "react-native-toast-message";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { Provider } from "react-redux";
+import store from "./src/store/store";
 
 const queryClient = new QueryClient();
 
@@ -14,11 +16,13 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
       <GestureHandlerRootView>
         <BottomSheetModalProvider>
-          <StatusBar backgroundColor="transparent" />
-          <SafeAreaView style={{ flex: 1 }}>
-            <RootNavigation />
-            <Toast />
-          </SafeAreaView>
+          <Provider store={store}>
+            <StatusBar backgroundColor="transparent" />
+            <SafeAreaView style={{ flex: 1 }}>
+              <RootNavigation />
+              <Toast />
+            </SafeAreaView>
+          </Provider>
         </BottomSheetModalProvider>
       </GestureHandlerRootView>
     </QueryClientProvider>
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,13 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import {
+  initializeAuth,
+  getReactNativePersistence,
+  getAuth,
+} from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,6 +25,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const auth = getAuth(app);
 export const storage = getStorage(app);
 
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
